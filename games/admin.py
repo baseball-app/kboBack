15fd8a2d8d349c3f7ad1df0530a3820fe360978f
SeoJimin <jnuni004@naver.com>
2024-06-01 00:13:16 +0900

    Create Game Team Ballpark Model

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Game)
+class Game(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Team)
+class Team(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Ballpark)
+class Ballpark(admin.ModelAdmin):
+    pass
--- games/models.py
+++ games/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=30)
+    logo =  models.ImageField(upload_to='images/')
+
+class Ballpark(models.Model):
+    name = models.CharField(max_length=50)
+    team = models.ForeignKey('games.Team', related_name='ballparks', on_delete = models.SET_NULL, null=True, blank=False)
+
+class Game(models.Model):
+    date = models.DateField()
+    team_home = models.ManyToManyField("games.Team", related_name ='home_games', blank=False)
+    team_away = models.ManyToManyField("games.Team", related_name ='away_games', blank=False)
+    ballpark = models.ManyToManyField("games.Ballpark", related_name="games", blank=False)
+    time = models.TimeField()
