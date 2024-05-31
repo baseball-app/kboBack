79bd28d7fa6b1e27bda1f41fa14aa80bc529f4eb
SeoJimin <jnuni004@naver.com>
2024-06-01 00:03:09 +0900

    Create Games App

--- kboBack/settings.py
+++ kboBack/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'tickets'
+    'tickets',
+    "games.apps.GamesConfig"
 ]
 
 MIDDLEWARE = [
