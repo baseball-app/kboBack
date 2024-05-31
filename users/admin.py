97f29e137d5c1974a445c5d22491c69ec7b2f0e1
SeoJimin <jnuni004@naver.com>
2024-05-31 23:53:08 +0900

    Add Admin Setting of Ticket and Users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.User)
+class CustomUser(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Friendship)
+class Frientship(admin.ModelAdmin):
+    pass
