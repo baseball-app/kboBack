07e370520fb5b41f343173f11c3a28f9c7a26042
Fabichoi <fabianus.c@gmail.com>
2024-10-15 22:59:28 +0900

    [KBOBACK-36] 소스 정리 및 oauth2 설정 추가

---
+++ backend/apis/users/swagger.py
@@ -0,0 +1,35 @@
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter
+
+SWAGGER_USERS_TAGS = ["USERS"]
+
+QUERY_PARAMETER_EMAIL = OpenApiParameter(
+    name="email",
+    type=str,
+    location=OpenApiParameter.QUERY,
+    description="이메일",
+    required=False,
+)
+
+QUERY_PARAMETER_PASSWORD = OpenApiParameter(
+    name="password",
+    type=str,
+    location=OpenApiParameter.QUERY,
+    description="비밀번호",
+    required=False,
+)
+
+SWAGGER_USERS_SIGN_UP = extend_schema(
+    tags=SWAGGER_USERS_TAGS,
+    summary="회원 가입",
+    description="회원에 가입합니다.",
+    parameters=[
+        QUERY_PARAMETER_EMAIL,
+        QUERY_PARAMETER_PASSWORD
+    ],
+)
+
+SWAGGER_USERS_ME = extend_schema(
+    tags=SWAGGER_USERS_TAGS,
+    summary="내 정보 확인",
+    description="내 정보를 확인 합니다.",
+)
