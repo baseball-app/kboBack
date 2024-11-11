bf286797faae31f5953b8847e4316575340457ad
leeceo97 <leeceo97@naver.com>
2024-11-11 21:47:03 +0900

    (add)notifications modeling

--- backend/apps/notifications/models.py
+++ backend/apps/notifications/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+
+from base.models import TimeStampModel
+
+
+class Notification(TimeStampModel):
+    user = models.ForeignKey("users.User", related_name="notifications", on_delete=models.CASCADE)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Notification for {self.user.nickname}"
+
+    class Meta:
+        ordering = ["-created_at"]
