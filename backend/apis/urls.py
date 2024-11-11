3408bea63ad63b3054859f7784091f8d2ab90024
leeceo97 <leeceo97@naver.com>
2024-11-11 21:40:24 +0900

    (feat)notifications initial settings

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('alerts/', include('apis.alerts.urls')),
-    path('auths/', include('apis.auths.urls')),
-    path('games/', include('apis.games.urls')),
-    path('tickets/', include('apis.tickets.urls')),
-    path('users/', include('apis.users.urls')),
+    path("alerts/", include("apis.alerts.urls")),
+    path("auths/", include("apis.auths.urls")),
+    path("games/", include("apis.games.urls")),
+    path("tickets/", include("apis.tickets.urls")),
+    path("users/", include("apis.users.urls")),
+    path("notifications/", include("apis.notifications.urls")),
 ]
