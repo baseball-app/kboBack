9f9a8c46bcfe3875a2d2fc80695fbc348193866b
오세윤\tpdbs <metro2641@gmail.com>
2024-12-11 20:12:45 +0900

    [KBO-BACK 56] 직관일기 리스트 조회 부분 추가(티켓 조회)

--- backend/apis/tickets/views.py
+++ backend/apis/tickets/views.py
@@ -1,15 +1,22 @@
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from apis.tickets.serializers import TicketSerializer
+from apps.tickets.models import Ticket
 
 class TicketsViewSet(
-    ListModelMixin,
-    CreateModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin,
     GenericViewSet,
 ):
     permission_classes = [
         AllowAny,
     ]
+
+    @action(methods=["GET"], detail=False, permission_classes=[IsAuthenticated])
+    def ticketListView(self, request):
+        user = request.user
+        queryset = Ticket.objects.filter(writer=user)
+        serializer = TicketSerializer(queryset, many=True)  # 쿼리셋 직렬화
+        return Response(serializer.data)
