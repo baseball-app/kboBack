504bc20830352dcfef5b8e998ac1b51d5c70babb
fabichoi <fabianus.c@gmail.com>
2024-12-16 23:06:09 +0900

    [KBOBACK-66] 팔로우, 언팔로우 API 추가

--- backend/apis/users/swagger.py
+++ backend/apis/users/swagger.py
@@ -1,4 +1,5 @@
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.utils import extend_schema, OpenApiParameter, inline_serializer
+from rest_framework import serializers
 
 SWAGGER_USERS_TAGS = ["USERS"]
 
@@ -18,8 +19,38 @@ QUERY_PARAMETER_PASSWORD = OpenApiParameter(
     required=False,
 )
 
+BODY_PARAMETER_FOR_FOLLOW = inline_serializer(
+    name="follow",
+    fields={
+        "source_id": serializers.IntegerField(),
+        "target_id": serializers.IntegerField(),
+    },
+)
+
+BODY_PARAMETER_FOR_UNFOLLOW = inline_serializer(
+    name="unfollow",
+    fields={
+        "source_id": serializers.IntegerField(),
+        "target_id": serializers.IntegerField(),
+    },
+)
+
 SWAGGER_USERS_ME = extend_schema(
     tags=SWAGGER_USERS_TAGS,
     summary="내 정보 확인",
     description="내 정보를 확인 합니다.",
+)
+
+SWAGGER_USERS_FOLLOW = extend_schema(
+    tags=SWAGGER_USERS_TAGS,
+    summary="친구 팔로우",
+    description="친구를 팔로우 합니다.",
+    request=BODY_PARAMETER_FOR_FOLLOW,
+)
+
+SWAGGER_USERS_UNFOLLOW = extend_schema(
+    tags=SWAGGER_USERS_TAGS,
+    summary="친구 언팔로우",
+    description="친구를 언팔로우 합니다.",
+    request=BODY_PARAMETER_FOR_UNFOLLOW,
 )
--- backend/apis/users/views.py
+++ backend/apis/users/views.py
@@ -1,15 +1,19 @@
 from drf_spectacular.utils import extend_schema_view
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from .serializers import UserInfoSerializer
-from .swagger import SWAGGER_USERS_ME
+from .serializers import UserInfoSerializer, UserFollowSerializer
+from .services import UserFollowService
+from .swagger import SWAGGER_USERS_ME, SWAGGER_USERS_FOLLOW, SWAGGER_USERS_UNFOLLOW
 
 
 @extend_schema_view(
     me=SWAGGER_USERS_ME,
+    follow=SWAGGER_USERS_FOLLOW,
+    unfollow=SWAGGER_USERS_UNFOLLOW,
 )
 class UsersViewSet(GenericViewSet):
 
@@ -17,3 +21,25 @@ class UsersViewSet(GenericViewSet):
     def me(self, request):
         serializer = UserInfoSerializer(request.user)
         return Response(serializer.data)
+
+    @action(methods=["POST"], detail=False, permission_classes=[IsAuthenticated])
+    def follow(self, request):
+        serializer = UserFollowSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        service = UserFollowService()
+        service.make_relation(data.get("source_id"), data.get("target_id"))
+
+        return Response(status=status.HTTP_200_OK)
+
+    @action(methods=["POST"], detail=False, permission_classes=[IsAuthenticated])
+    def unfollow(self, request):
+        serializer = UserFollowSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        service = UserFollowService()
+        service.release_relation(data.get("source_id"), data.get("target_id"))
+
+        return Response(status=status.HTTP_200_OK)
