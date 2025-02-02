5ca59a6f1adc542e5bc9fb0e15977df8cdf226ea
fabichoi <fabianus.c@gmail.com>
2025-02-02 21:14:47 +0900

    FIX: ApiValidation 관련 수정

--- backend/apis/exceptions.py
+++ backend/apis/exceptions.py
@@ -5,17 +5,6 @@ from rest_framework.exceptions import APIException, _get_error_details
 class ApiValidationError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
 
-    def __init__(self, err_code, err_msg=None):
-        self.err_code = err_code
-        self.err_msg = err_msg
-
-    def __str__(self):
-        return self.err_code
-
-
-class CustomValidationError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
-
     def __init__(self, detail=None, extras=None, code=None):
         if detail is None:
             detail = self.default_detail
