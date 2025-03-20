c621c6e832961746f598c1f37a2516840ba96bde
Fabi H. Choi <fabianus.c@gmail.com>
2025-03-20 21:58:57 +0900

    FIX: 센트리 미들웨어 롤백 - 무관한 부분인 것 확인

--- backend/base/middlewares.py
+++ backend/base/middlewares.py
@@ -7,19 +7,5 @@ class SentryTransactionMiddleware:
 
     def __call__(self, request):
         with sentry_sdk.start_transaction(op="http.request", name=request.path):
-            post_data = None
-            if request.method in ["POST", "PUT", "PATCH"]:
-                try:
-                    post_data = request.body.decode("utf-8")
-                except Exception:
-                    post_data = "[Unreadable]"
-
-            sentry_sdk.set_context("request", {
-                "method": request.method,
-                "url": request.build_absolute_uri(),
-                "headers": dict(request.headers),
-                "post_data": post_data,
-            })
-
             response = self.get_response(request)
             return response
