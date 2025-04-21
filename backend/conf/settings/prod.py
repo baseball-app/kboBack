eb4f574b1df653899c87d699f452f603de6c902a
Fabi H. Choi <fabianus.c@gmail.com>
2025-04-21 09:47:17 +0900

    Update prod.py

--- backend/conf/settings/prod.py
+++ backend/conf/settings/prod.py
@@ -1,3 +1,8 @@
 from conf.settings.base import *  # noqa: F403
 
 DEBUG = False
+ALLOWED_HOSTS = ['kboapp.xyz', 'www.kboapp.xyz', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://kboapp.xyz',
+]
+DEFAULT_HOST = "https://kboapp.xyz"
