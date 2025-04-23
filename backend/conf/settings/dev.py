5a5b07d8e68254f4291085535b6a71f47fbfe8e0
leeceo97 <leeceo97@naver.com>
2025-04-23 16:49:01 +0900

    (fix)static file settings

--- backend/conf/settings/dev.py
+++ backend/conf/settings/dev.py
@@ -1,8 +1,11 @@
 from conf.settings.base import *  # noqa: F403
 
 DEBUG = False
-ALLOWED_HOSTS = ['kboapp.xyz', 'www.kboapp.xyz', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["kboapp.xyz", "www.kboapp.xyz", "localhost", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = [
-    'https://kboapp.xyz',
+    "https://kboapp.xyz",
 ]
 DEFAULT_HOST = "https://kboapp.xyz"
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  # noqa: F405
